7eb4a502d5318b61a83f6c8c32384e2cc8f904f2
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-06-21 11:18:56 +0200

    Creating method create in movieNotes

---
+++ src/controllers/MovieNotesController.js
@@ -0,0 +1,25 @@
+const knex = require('../database/knex');
+const AppError = require('../utils/AppError');
+
+class MovieNotesController {
+  async create(request, response) {
+    const { title, description, rating } = request.body;
+    const { user_id } = request.params;
+
+    if(!rating || rating < 1 || rating > 5) {
+      throw new AppError('A avaliação precisa ser um número entre 1 e 5');
+    }
+    
+    const [movieNote_id] = await knex('movieNotes').insert({
+      title,
+      description,
+      rating,
+      user_id
+    });
+
+    return response.json({ movieNote_id });
+     
+  }
+}
+
+module.exports = MovieNotesController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 
-const usersRouter = require("./Users.routes")
+const usersRouter = require("./Users.routes");
+const movieNotesRouter = require("./MovieNotes.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
+routes.use("/movieNotes", movieNotesRouter);
 
 module.exports = routes;
