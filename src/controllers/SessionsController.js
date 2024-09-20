48133e82554cb906865b42b2b6f6047e5193f5fa
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-09-20 10:52:34 +0200

    Creating token generation for each user

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,8 @@
 const knex = require("../database/knex");
 const AppError = require('../utils/AppError');
-const { compare } = require("bcryptjs")
+const { compare } = require("bcryptjs");
+const authConfig = require("../configs/auth");
+const { sign } = require("jsonwebtoken")
 
 class SessionsController {
   async create(request, response) {
@@ -17,8 +19,14 @@ class SessionsController {
     if(!passwordMatch) {
       throw new AppError("E-mail e/ou senha incorreta", 401);
     }
+    const { secret, expiresIn } = authConfig.jwt;
 
-    return response.json({ user })
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    })
+
+    return response.json({ user, token })
   }
 
 }
