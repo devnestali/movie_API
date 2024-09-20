20822c63f42f6717861d563ffaa13dd9c25337f8
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-09-20 09:40:07 +0200

    Creating sessionRoutes file and sessionControler file

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require("express");
 const usersRouter = require("./Users.routes");
 const movieNotesRouter = require("./MovieNotes.routes");
 const movieTagsRouter = require("./MovieTags.routes");
+const sessionsRouter = require("./Sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
+routes.use("/sessions", sessionsRouter);
 routes.use("/movieNotes", movieNotesRouter);
 routes.use("/movieTags", movieTagsRouter);
 
