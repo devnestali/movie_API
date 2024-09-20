e8584ec602c040d3e34f7a024db4fe328da69bd9
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-09-20 10:18:45 +0200

    Searching the database for the user password and checking if this user password exists

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,14 +1,21 @@
 const knex = require("../database/knex");
 const AppError = require('../utils/AppError');
+const { compare } = require("bcryptjs")
 
 class SessionsController {
   async create(request, response) {
     const { email, password } = request.body;
 
     const user = await knex("users").where({ email }).first();
-
+    
     if(!user) {
-      throw new AppError("Usuário não encontrado", 401);
+      throw new AppError("E-mail e/ou senha incorreta", 401);
+    }
+    
+    const passwordMatch = await compare(password, user.password);
+
+    if(!passwordMatch) {
+      throw new AppError("E-mail e/ou senha incorreta", 401);
     }
 
     return response.json({ user })
