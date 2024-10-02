070935f9afd1fac3251490203da9fbbd2d0a3cd9
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-10-02 16:05:41 +0200

    updating knexfile and deleting tables and creating new table createUsers

--- knexfile.js
+++ knexfile.js
@@ -7,12 +7,18 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
-    pool: {
-      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON;", cb)
-    },
     migrations: {
-      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+      directory: path.resolve(
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      ),
+    },
+    pool: {
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys=ON", cb)
     },
     useNullAsDefault: true
-  }
+  },
 };
